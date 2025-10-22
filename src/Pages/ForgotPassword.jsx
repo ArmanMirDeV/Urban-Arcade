d276ffc7df70dcd5bbd118b25b847aed1b06e344
Arman Mir <mirarman8583@gmail.com>
2025-10-22 08:38:23 +0600

    refactor: remove old App component and CSS, implement routing with MainLayout and initial pages

---
+++ src/Pages/ForgotPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ForgotPassword = () => {
+    return (
+        <div>
+            <h2>ForGot password here</h2>
+        </div>
+    );
+};
+
+export default ForgotPassword;
---
+++ src/Pages/GameDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GameDetails = () => {
+    return (
+        <div>
+         <h2>Game details are here</h2>   
+        </div>
+    );
+};
+
+export default GameDetails;
