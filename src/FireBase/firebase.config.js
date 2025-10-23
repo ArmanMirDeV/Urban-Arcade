6d7a2d45f1ba3d26c8e65730399502de4bf02046
Arman Mir <mirarman8583@gmail.com>
2025-10-23 11:58:41 +0600

    feat: update Firebase configuration to use environment variables for sensitive data

--- src/FireBase/firebase.config.js
+++ src/FireBase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDQ6-VPP8L_0RoZfXyalhYDNEjsC2hoIkM",
-  authDomain: "urban-arcade-8fc90.firebaseapp.com",
-  projectId: "urban-arcade-8fc90",
-  storageBucket: "urban-arcade-8fc90.firebasestorage.app",
-  messagingSenderId: "58846991229",
-  appId: "1:58846991229:web:a32baa6820391ee5fb3214",
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
